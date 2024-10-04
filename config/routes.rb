30ceae899ca1e788a6fc0d650318332a71f4f77c
jeonginh0 <stitch010913@gmail.com>
2024-10-04 05:38:18 +0000

    2nd commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  resources :articles do
+	resources :comments, only: [:create] #댓글이 게시글에 종속되도록 설정
+  end
 	
+  resources :class_statuses
+  resources :class_lists
   devise_for :users
   resources :tests
-	resources :tests
 		
-		root 'tests#index'
+  # root 'tests#index'
+  root 'articles#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

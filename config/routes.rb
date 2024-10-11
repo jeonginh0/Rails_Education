dc3c982c1ead82638a436bb1fa24a90533dbff4a
jeonginh0 <stitch010913@gmail.com>
2024-10-11 11:24:06 +0000

    product, review 1:N

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :articles do
-	resources :comments, only: [:create] #댓글이 게시글에 종속되도록 설정
+  # resources :reviews
+  resources :products do
+	resources :reviews, only: [:create] #리뷰가 상품에 종속되도록 설정
   end
+	# resources :articles do
+	# resources :comments, only: [:create] #댓글이 게시글에 종속되도록 설정
+	# end
 	
   resources :class_statuses
   resources :class_lists
@@ -9,6 +13,7 @@ Rails.application.routes.draw do
   resources :tests
 		
   # root 'tests#index'
-  root 'articles#index'
+  # root 'articles#index'
+	root 'products#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_10_04_042203) do
+ActiveRecord::Schema.define(version: 2024_10_11_085209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,23 @@ ActiveRecord::Schema.define(version: 2024_10_04_042203) do
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.integer "rating"
+    t.bigint "product_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["product_id"], name: "index_reviews_on_product_id"
+  end
+
   create_table "tests", force: :cascade do |t|
     t.string "name"
     t.integer "age"
@@ -57,4 +74,5 @@ ActiveRecord::Schema.define(version: 2024_10_04_042203) do
   end
 
   add_foreign_key "comments", "articles"
+  add_foreign_key "reviews", "products"
 end
